94149e05ae54659131376a3ae6986f35066ac37f
linzehuan1996 <linzehuan1996@outlook.com>
2017-07-30 10:52:56 +0800

    ex-1.18

---
+++ chapter1/ex-18.scm
@@ -0,0 +1,10 @@
+(define (double n) (* n 2))
+(define (halve n) (/ n 2))
+(define (is_even n) (= (remainder n 2) 0))
+
+(define (fast_product a b)
+    (define (fast_product_iter x y accumulator)
+        (cond ((= y 0) accumulator)
+              ((is_even y) (fast_product_iter (double x) (halve y) accumulator))
+              (else (fast_product_iter x (- y 1) (+ accumulator x)))))
+    (fast_product_iter a b 0))
