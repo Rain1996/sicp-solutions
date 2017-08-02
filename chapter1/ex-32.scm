0208b428a87048595cce90460be2c08573d07c54
linzehuan1996 <linzehuan1996@outlook.com>
2017-08-02 23:38:14 +0800

    ex-1.32

---
+++ chapter1/ex-32.scm
@@ -0,0 +1,19 @@
+(define (accumulate combiner null_value term a next b)
+    (if (> a b)
+        null_value
+        (combiner (term a)
+                  (accumulate combiner null_value term (next a) next b))))
+
+(define (accumulate_iter combiner null_value term a next b)
+    (define (iter a result)
+        (if (> a b)
+            result
+            (iter (next a) (combiner result (term a)))))
+    (iter a null_value))
+
+
+(define (sum term a next b)
+    (accumulate + 0 term a next b))
+
+(define (product term a next b)
+    (accumulate_iter * 1 term a next b))
