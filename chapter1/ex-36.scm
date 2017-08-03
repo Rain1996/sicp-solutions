33ee29e7a386c18381f865e60ba3678f4abf4e87
linzehuan1996 <linzehuan1996@outlook.com>
2017-08-03 22:49:52 +0800

    ex-1.36

---
+++ chapter1/ex-36.scm
@@ -0,0 +1,17 @@
+(define tolerance 0.00001)
+
+(define (fixed_point f first_guess)
+    (define (close_enough? v1 v2)
+        (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+        (newline)
+        (display guess)
+        (let ((next (f guess)))
+            (if (close_enough? guess next)
+                next
+                (try next))))
+    (try first_guess))
+
+(fixed_point (lambda (x) (/ (log 1000) (log x))) 2)
+
+(fixed_point (lambda(x) (/ (+ x (/ (log 1000) (log x))) 2)) 2)
