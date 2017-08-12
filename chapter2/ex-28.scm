0d5fd528f37e8f1fb34212a2784c7d5206246887
linzehuan1996 <linzehuan1996@outlook.com>
2017-08-12 15:27:45 +0800

    ex-2.28

--- chapter2/ex-28.scm
+++ chapter2/ex-28.scm
@@ -7,3 +7,10 @@
 (define x (list (list 1 2) (list 3 4)))
 (fringe x)
 (fringe (list x x))
+
+; 被我搞复杂了, 下面这个简单点
+(define (fringe tree)
+    (cond ((null? tree) '())
+          ((pair? (car tree))
+                (append (fringe (car tree)) (fringe (cdr tree))))
+          (else (list tree))))
