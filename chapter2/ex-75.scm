d5b44d92703e8a68e3ca3781c31c92aff6f92601
linzehuan1996 <linzehuan1996@outlook.com>
2017-08-26 23:36:29 +0800

    ex-2.75

---
+++ chapter2/ex-75.scm
@@ -0,0 +1,8 @@
+(define (make_from_mag_ang r a)
+    (define (dispatch op)
+        (cond ((eq? op 'real_part) (* r (cos a)))
+              ((eq? op 'imag_part) (* r (sin a)))
+              ((eq? op 'magnitude) r)
+              ((eq? op 'angle) a)
+              (else "Unknow op -- MAKE_FROM_MAG_ANG" op)))
+    dispatch)
