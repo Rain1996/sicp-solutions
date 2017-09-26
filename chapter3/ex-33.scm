afaec54b42b12a82cc91f089f2129a404780b580
“linzehuan <linzehuan1996@outlook.com>
2017-09-26 14:20:52 +0800

    ex-3.33

--- chapter3/ex-33.scm
+++ chapter3/ex-33.scm
@@ -1,7 +1,26 @@
 (define (average_convert a b c)
-    (let ((sum (make_connector)
-          (count (make_connector))))
+    (let ((sum (make_connector))
+          (count (make_connector)))
         (adder a b sum)
         (multiplier c count sum)
         (constant 2 count)
         'ok))
+
+; test
+(define A (make_connector))
+(define B (make_connector))
+(define C (make_connector))
+
+(average_convert A B C)
+
+(probe "A" A)
+(probe "B" B)
+(probe "C" C)
+
+(set_value! A 25 'user)
+(set_value! B 15 'user)
+
+(forget_value! A 'user)
+
+(set_value! C 50 'user)
+(set_value! A 40 'user)
