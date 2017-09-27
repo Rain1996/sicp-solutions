3947bc00528eca06df74e6d0e0708772959e0679
“linzehuan <linzehuan1996@outlook.com>
2017-09-27 08:56:53 +0800

    ex-3.38

---
+++ chapter3/ex-38.scm
@@ -0,0 +1,8 @@
+; a)
+; 有6种可能
+; Peter (110) -> Paul (90) -> Mary (45)
+; Peter (110) -> Mary (55) -> Paul (35)
+; Paul (80) -> Peter (90) -> Mary (45)
+; Paul (80) -> Mary (40) -> Peter (50)
+; Mary (50) -> Peter (60) -> Paul (40)
+; Mary (50) -> Paul (30) -> Peter (40)
