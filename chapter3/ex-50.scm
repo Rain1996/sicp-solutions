fbaf8186d27f536942ae7cc242f2471f0a566858
“linzehuan <linzehuan1996@outlook.com>
2017-10-11 08:20:24 +0800

    ex-3.50

--- chapter3/ex-50.scm
+++ chapter3/ex-50.scm
@@ -1,5 +1,5 @@
 (define (stream_map proc . argstreams)
-    (if (null? (car argstreams))
+    (if (stream_null? (car argstreams))
         the_empty_stream
         (begin (apply proc (map stream_car argstreams))
                (apply proc (map stream_cdr argstreams)))))
