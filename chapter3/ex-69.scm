78abb1095d136ffe5c1ccff99971f30a671052d4
“linzehuan <linzehuan1996@outlook.com>
2017-10-20 08:42:47 +0800

    ex-3.69

---
+++ chapter3/ex-69.scm
@@ -0,0 +1,10 @@
+(define (triples s t u)
+    (cons-stream (list (stream-car s) (stream-car t) (stream-car u))
+                 (interleave (stream-map (lambda (x) (cons (stream-car s) x)) (stream-cdr (pairs t u)))
+                             (triples (stream-cdr s) (stream-cdr t) (stream-cdr u)))))
+
+(define (phythagorean_number)
+    (let ((numbers (triples integers integers integers)))
+        (stream-filter (lambda (x) (= (+ (square (car x) (square (cadr x))))
+                                      (square (caddr x))))
+                       numbers)))
