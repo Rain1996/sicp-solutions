51bb30e1b11dbddca07cd3af32584b1f8d17b10c
“linzehuan <linzehuan1996@outlook.com>
2017-10-24 08:53:13 +0800

    ex-3.76

--- chapter3/ex-76.scm
+++ chapter3/ex-76.scm
@@ -1,6 +1,9 @@
 (define (smooth s)
     (stream-map (lambda (x y) (/ (+ x y) 2)) s (cons-stream 0 s)))
 
-(define (make_smooth_zero_crossings smooth sense_data)
-    (let ((smooth_sense_data (smooth sense_data)))
+(define (make_smooth_zero_crossings smooth_fun sense_data)
+    (let ((smooth_sense_data (smooth_fun sense_data)))
         (stream-map sign-change-detector smooth_sense_data (cons-stream 0 smooth_sense_data))))
+
+(define smooth_zero_crossings
+    (make_smooth_zero_crossings smooth sense_data))
