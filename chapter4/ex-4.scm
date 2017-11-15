d38985e49b789e75866e78babc980a22422a8cb3
linzehuan <linzehuan1996@outlook.com>
2017-11-15 09:26:25 +0800

    ex-4.4

--- chapter4/ex-4.scm
+++ chapter4/ex-4.scm
@@ -29,10 +29,10 @@
     (eval-and-helper (and-clauses exp) env))
 
 (define (eval-and-helper clauses env)
-    (let ((first (and-first-clause clauses))
-          (rest (and-rest-clause clauses)))
-        (if (null? clauses)
-            'true
+    (if (null? clauses)
+        'true
+        (let ((first (and-first-clause clauses))
+              (rest (and-rest-clause clauses)))
             (if (true? (eval first env))
                 (eval-and-helper rest env)
                 'false))))
@@ -47,10 +47,65 @@
     (eval-or-helper (or-clauses exp) env))
 
 (define (eval-or-helper clauses env)
-    (let ((first (or-first-clause clauses))
-          (rest (or-rest-clause clauses)))
-        (if (null? clauses)
-            'false
+    (if (null? clauses)
+        'false
+        (let ((first (or-first-clause clauses))
+              (rest (or-rest-clause clauses)))
             (if (true? (eval first env))
                 'true
                 (eval-or-helper rest env)))))
+
+
+; implement and and or as derived expressions
+(define (eval exp env)
+(cond ((self-evaluating? exp) exp)
+      ((variable? exp) (lookup-variable-value exp env))
+      ((quoted? exp) (text-of-quotation exp))
+      ((assignment? exp) (eval-assignment exp env))
+      ((definition? exp) (eval-definition exp env))
+      ((if? exp) (eval-if exp env))
+      ((lambda? exp)
+            (make-procedure (lambda-parameters exp)
+                            (lambda-body exp)
+                            env))
+      ((begin? exp)
+            (eval-sequence (begin-actions exp) env))
+      ((cond? exp) (eval (cond->if exp) env))
+      ((and? exp) (eval (and->if exp) env))       ; special forms and
+      ((or? exp) (eval (or->if exp) env))         ; special forms or
+      ((application? exp)
+            (apply (eval (operator exp) env)
+                   (list-of-values (operands exp) env)))
+      (else (error "Unknown expression type -- EVAL" exp))))
+
+; special forms and
+(define (and? exp) (tagged-list? exp 'and))
+(define (and-clauses exp) (cdr exp))
+(define (and-first-clause clauses) (car clauses))
+(define (and-rest-clause clauses) (cdr clauses))
+
+(define (and->if exp)
+(and->if-helper (and-clauses exp)))
+
+(define (and->if-helper clauses)
+    (if (null? clauses)
+        'true
+        (let ((first (and-first-clause clauses))
+              (rest (and-rest-clause clauses)))
+            (make-if first (cons 'and rest) 'false))))
+
+; special forms or
+(define (or? exp) (tagged-list? exp 'or))
+(define (or-clauses exp) (cdr exp))
+(define (or-first-clause clauses) (car clauses))
+(define (or-rest-clause clauses) (cdr clauses))
+
+(define (eval-or exp env)
+(eval-or-helper (or-clauses exp) env))
+
+(define (eval-or-helper clauses env)
+(if (null? clauses)
+    'false
+    (let ((first (or-first-clause clauses))
+          (rest (or-rest-clause clauses)))
+        (make-if first 'ture (cons 'or  rest)))))
